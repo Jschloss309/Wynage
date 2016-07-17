d4243511c40a4c85a820a6e87c495b367bd974ff
Jonathon Schloss <Jschloss309@gmail.com>
2016-07-17 18:32:47 -0400

    Changed home page to posts#all

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :users
   get 'page/index'
-  root 'page#index'
+  root 'posts#all'
   resources :pictures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
